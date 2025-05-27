8f0a8da0710afb6e24492f889a7ed3cca3f840dd
Vincentius <vi.farenden@duocuc.cl>
2025-05-27 19:02:51 -0400

    test

--- ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/model/solicitud.java
+++ ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/model/solicitud.java
@@ -1,13 +1,23 @@
 package ReparaFacilV1.ReparaFacil.model;
 
-import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Entity
 @Data
