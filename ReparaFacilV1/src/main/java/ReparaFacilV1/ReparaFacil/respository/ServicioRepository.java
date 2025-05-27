96a202c9e54f3cf7dbd920066c43511335aaa57b
Marbeck-one <ro.martinezb@duocuc.cl>
2025-05-27 19:01:08 -0400

    se integra fetch ID oara servicio

--- ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/respository/ServicioRepository.java
+++ ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/respository/ServicioRepository.java
@@ -10,8 +10,16 @@ import main.java.ReparaFacilV1.ReparaFacil.model.Pago;
 import main.java.ReparaFacilV1.ReparaFacil.model.Tecnico;
 import main.java.ReparaFacilV1.ReparaFacil.model.Valoracion;
 
+import java.util.List;
+
+import ReparaFacilV1.ReparaFacil.model.Servicio;
+
 
 @Respository
 public interface ServicioRepository extends JpaRepository<Servicio, Cliente, Pago, Solicitud, Tecnico, Valoracion, Long> {
+
+    public static List<Servicio> findALL();
+
+    public Object findById(Long id);
 
     }
--- ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/service/ServicioService.java
+++ ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/service/ServicioService.java
@@ -1,9 +1,12 @@
 package main.java.ReparaFacilV1.ReparaFacil.service;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 import org.springframework.stereotype.Service;
 
+import ReparaFacilV1.ReparaFacil.model.*;
 import jakarta.transaction.Transactional;
 import main.java.ReparaFacilV1.ReparaFacil.respository.ServicioRepository;
 
@@ -16,4 +19,14 @@ public class ServicioService {
     @Autowired
     private ServicioRepository servicioRepository;
 
+    public List<Servicio> fetchALL() {
+
+        return ServicioRepository.findALL();
+
+    }
+
+    public Servicio fetchById(Long id){
+
+        return servicioRepository.findById(id).get();
+    }
 }
