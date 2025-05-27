e23c22b56e271a23e81053adbff086a0020dd8a2
Thragg <mass.navarrete@duocuc.cl>
2025-05-26 22:39:20 -0400

    GIT CLONE

---
+++ ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/ReparaFacilV1Application.java
@@ -0,0 +1,13 @@
+package ReparaFacilV1.ReparaFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReparaFacilV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReparaFacilV1Application.class, args);
+	}
+
+}
---
+++ ReparaFacilV1/src/test/java/ReparaFacilV1/ReparaFacil/ReparaFacilV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ReparaFacilV1.ReparaFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReparaFacilV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
