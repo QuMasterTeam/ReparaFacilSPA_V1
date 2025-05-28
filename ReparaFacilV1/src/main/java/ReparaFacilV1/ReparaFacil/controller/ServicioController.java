bcd4c89e0db90b9b86940e9b030e889225f9a2ce
Marbeck-one <ro.martinezb@duocuc.cl>
2025-05-27 21:32:39 -0400

    Controller listo Video 1:30:00

--- ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/controller/ServicioController.java
+++ ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/controller/ServicioController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ReparaFacilV1.ReparaFacil.model.Servicio;
+
 import java.ReparaFacilV1.ReparaFacil.service.ServicioService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 
@@ -20,7 +25,7 @@ public class ServicioController {
     private ServicioService servicioService;
 
     @GetMapping
-    public ResponseEntity<List<Servicio>> Listar(){
+    public ResponseEntity<List<Servicio>> listar(){
         List<Servicio> servicios = servicioService.fetchALL();
         
         if (servicios.isEmpty()) {
@@ -31,24 +36,22 @@ public class ServicioController {
         // status 200 ok
         return ResponseEntity.ok(servicios);
     }
-}
 
-@GetMapping()
-public class ServicioController {
-
-    @Autowired
-    private ServicioService servicioService;
-
-    public ResponseEntity<List<Servicio>> buscar(){
-        List<Servicio> servicios = servicioService.fetchALL();
+    @GetMapping("/{id}")
+    public ResponseEntity<Servicio> buscar(@PathVariable Integer id) {
+   
+        Servicio servicio = servicioService.fetchById(id);
         
-        if (servicios.isEmpty()) {
-            // error 404 not found
-            return ResponseEntity.noContent().build();
-        }
-
         // status 200 ok
-        return ResponseEntity.ok(servicios);
+        return ResponseEntity.ok(servicio);
     }
-}
 
+    @PostMapping
+    public ResponseEntity<Servicio> guardar(@RequestBody Servicio servicio) {
+        Servicio servicioMuevo = servicioService.save(servicio);
+        return ResponseEntity.status(HttpStatus.CREATED).body(servicioMuevo);
+        // return new responseEntity<> (prductoMuevo, HttpStatus.ACcepted);
+        
+    }
+
+}
