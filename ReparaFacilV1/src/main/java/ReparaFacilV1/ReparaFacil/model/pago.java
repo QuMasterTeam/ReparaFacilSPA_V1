a607abe418d553debde9414462da637c39a6a485
Vincentius <vi.farenden@duocuc.cl>
2025-05-27 21:11:50 -0400

    Se optimiza la documentación de Jakarta y se corrige modelo pago.java a Pago.java

--- ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/model/pago.java
+++ ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/model/pago.java
@@ -14,8 +14,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-
 @Entity
 @Data
 @NoArgsConstructor
--- ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/model/valoracion.java
+++ ReparaFacilV1/src/main/java/ReparaFacilV1/ReparaFacil/model/valoracion.java
@@ -1,8 +1,15 @@
-package ReparaFacilV1.ReparaFacil.Valoracion.model;
+package ReparaFacilV1.ReparaFacil.model;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
